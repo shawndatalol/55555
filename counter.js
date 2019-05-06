91b8d91cc1bd5222f3e2e79f5418c3b3297c0e89
shawn.chao <46641203+shawndatalol@users.noreply.github.com>
2019-05-06 16:29:01 +0800

    Add files via upload

---
+++ counter.js
@@ -0,0 +1,641 @@
+const state = {
+  current: 0
+};
+function increment() {
+  state.current++;
+}
+function reset(){
+  state.current = 0;
+}
+function discrement() {
+  state.current--;
+}
+
+document.getElementById('incrementButton').addEventListener(
+  'click',
+  function (){
+    increment();
+    render();
+  }
+);
+
+document.getElementById('resetButton').addEventListener(
+  'click',
+  function (){
+    reset();
+    render();
+  }
+);
+document.getElementById('discrementButton').addEventListener(
+  'click',
+  function () {
+    discrement();
+    render();
+  }
+)
+
+const state1 = {
+  current: 0
+};
+function increment1() {
+  state1.current++;
+}
+function reset1() {
+  state1.current = 0;
+}
+function discrement1() {
+  state1.current--;
+}
+
+
+
+document.getElementById('increment1Button').addEventListener(
+  'click',
+  function () {
+    increment1();
+    render();
+  }
+);
+
+document.getElementById('reset1Button').addEventListener(
+  'click',
+  function () {
+    reset1();
+    render();
+  }
+);
+document.getElementById('discrement1Button').addEventListener(
+  'click',
+  function () {
+    discrement1();
+    render();
+  }
+);
+const state2 = {
+  current: 0
+};
+function increment2() {
+  state2.current++;
+}
+function reset2() {
+  state2.current = 0;
+}
+function discrement2() {
+  state2.current--;
+}
+
+
+
+document.getElementById('increment2Button').addEventListener(
+  'click',
+  function () {
+    increment2();
+    render();
+  }
+);
+
+document.getElementById('reset2Button').addEventListener(
+  'click',
+  function () {
+    reset2();
+    render();
+  }
+);
+document.getElementById('discrement2Button').addEventListener(
+  'click',
+  function () {
+    discrement2();
+    render();
+  }
+);
+const state3 = {
+  current: 0
+};
+function increment3() {
+  state3.current++;
+}
+function reset3() {
+  state3.current = 0;
+}
+function discrement3() {
+  state3.current--;
+}
+
+
+
+document.getElementById('increment3Button').addEventListener(
+  'click',
+  function () {
+    increment3();
+    render();
+  }
+);
+
+document.getElementById('reset3Button').addEventListener(
+  'click',
+  function () {
+    reset3();
+    render();
+  }
+);
+document.getElementById('discrement3Button').addEventListener(
+  'click',
+  function () {
+    discrement3();
+    render();
+  }
+);
+const state4 = {
+  current: 0
+};
+function increment4() {
+  state4.current++;
+}
+function reset4() {
+  state4.current = 0;
+}
+function discrement4() {
+  state4.current--;
+}
+
+
+
+document.getElementById('increment4Button').addEventListener(
+  'click',
+  function () {
+    increment4();
+    render();
+  }
+);
+
+document.getElementById('reset4Button').addEventListener(
+  'click',
+  function () {
+    reset4();
+    render();
+  }
+);
+document.getElementById('discrement4Button').addEventListener(
+  'click',
+  function () {
+    discrement4();
+    render();
+  }
+);
+const state5 = {
+  current: 0
+};
+function increment5() {
+  state5.current++;
+}
+function reset5() {
+  state5.current = 0;
+}
+function discrement5() {
+  state5.current--;
+}
+
+
+
+document.getElementById('increment5Button').addEventListener(
+  'click',
+  function () {
+    increment5();
+    render();
+  }
+);
+
+document.getElementById('reset5Button').addEventListener(
+  'click',
+  function () {
+    reset5();
+    render();
+  }
+);
+document.getElementById('discrement5Button').addEventListener(
+  'click',
+  function () {
+    discrement5();
+    render();
+  }
+);
+const state6 = {
+  current: 0
+};
+function increment6() {
+  state6.current++;
+}
+function reset6() {
+  state6.current = 0;
+}
+function discrement6() {
+  state6.current--;
+}
+
+
+
+document.getElementById('increment6Button').addEventListener(
+  'click',
+  function () {
+    increment6();
+    render();
+  }
+);
+
+document.getElementById('reset6Button').addEventListener(
+  'click',
+  function () {
+    reset6();
+    render();
+  }
+);
+document.getElementById('discrement6Button').addEventListener(
+  'click',
+  function () {
+    discrement6();
+    render();
+  }
+);
+const state7 = {
+  current: 0
+};
+function increment7() {
+  state7.current++;
+}
+function reset7() {
+  state7.current = 0;
+}
+function discrement7() {
+  state7.current--;
+}
+
+
+
+document.getElementById('increment7Button').addEventListener(
+  'click',
+  function () {
+    increment7();
+    render();
+  }
+);
+
+document.getElementById('reset7Button').addEventListener(
+  'click',
+  function () {
+    reset7();
+    render();
+  }
+);
+document.getElementById('discrement7Button').addEventListener(
+  'click',
+  function () {
+    discrement7();
+    render();
+  }
+);
+const state8 = {
+  current: 0
+};
+function increment8() {
+  state8.current++;
+}
+function reset8() {
+  state8.current = 0;
+}
+function discrement8() {
+  state8.current--;
+}
+
+
+
+document.getElementById('increment8Button').addEventListener(
+  'click',
+  function () {
+    increment8();
+    render();
+  }
+);
+
+document.getElementById('reset8Button').addEventListener(
+  'click',
+  function () {
+    reset8();
+    render();
+  }
+);
+document.getElementById('discrement8Button').addEventListener(
+  'click',
+  function () {
+    discrement8();
+    render();
+  }
+);
+const state9 = {
+  current: 0
+};
+function increment9() {
+  state9.current++;
+}
+function reset9() {
+  state9.current = 0;
+}
+function discrement9() {
+  state9.current--;
+}
+
+
+
+document.getElementById('increment9Button').addEventListener(
+  'click',
+  function () {
+    increment9();
+    render();
+  }
+);
+
+document.getElementById('reset9Button').addEventListener(
+  'click',
+  function () {
+    reset9();
+    render();
+  }
+);
+document.getElementById('discrement9Button').addEventListener(
+  'click',
+  function () {
+    discrement9();
+    render();
+  }
+);
+const state10 = {
+  current: 0
+};
+function increment10() {
+  state10.current++;
+}
+function reset10() {
+  state10.current = 0;
+}
+function discrement10() {
+  state10.current--;
+}
+
+
+
+document.getElementById('increment10Button').addEventListener(
+  'click',
+  function () {
+    increment10();
+    render();
+  }
+);
+
+document.getElementById('reset10Button').addEventListener(
+  'click',
+  function () {
+    reset10();
+    render();
+  }
+);
+document.getElementById('discrement10Button').addEventListener(
+  'click',
+  function () {
+    discrement10();
+    render();
+  }
+);
+const state11 = {
+  current: 0
+};
+function increment11() {
+  state11.current++;
+}
+function reset11() {
+  state11.current = 0;
+}
+function discrement11() {
+  state11.current--;
+}
+
+
+
+document.getElementById('increment11Button').addEventListener(
+  'click',
+  function () {
+    increment11();
+    render();
+  }
+);
+
+document.getElementById('reset11Button').addEventListener(
+  'click',
+  function () {
+    reset11();
+    render();
+  }
+);
+document.getElementById('discrement11Button').addEventListener(
+  'click',
+  function () {
+    discrement11();
+    render();
+  }
+);
+const state12 = {
+  current: 0
+};
+function increment12() {
+  state12.current++;
+}
+function reset12() {
+  state12.current = 0;
+}
+function discrement12() {
+  state12.current--;
+}
+
+
+
+document.getElementById('increment12Button').addEventListener(
+  'click',
+  function () {
+    increment12();
+    render();
+  }
+);
+
+document.getElementById('reset12Button').addEventListener(
+  'click',
+  function () {
+    reset12();
+    render();
+  }
+);
+document.getElementById('discrement12Button').addEventListener(
+  'click',
+  function () {
+    discrement12();
+    render();
+  }
+);
+const state13 = {
+  current: 0
+};
+function increment13() {
+  state13.current++;
+}
+function reset13() {
+  state13.current = 0;
+}
+function discrement13() {
+  state13.current--;
+}
+
+
+
+document.getElementById('increment13Button').addEventListener(
+  'click',
+  function () {
+    increment13();
+    render();
+  }
+);
+
+document.getElementById('reset13Button').addEventListener(
+  'click',
+  function () {
+    reset13();
+    render();
+  }
+);
+document.getElementById('discrement13Button').addEventListener(
+  'click',
+  function () {
+    discrement13();
+    render();
+  }
+);
+const state14 = {
+  current: 0
+};
+function increment14() {
+  state14.current++;
+}
+function reset14() {
+  state14.current = 0;
+}
+function discrement14() {
+  state14.current--;
+}
+
+
+
+document.getElementById('increment14Button').addEventListener(
+  'click',
+  function () {
+    increment14();
+    render();
+  }
+);
+
+document.getElementById('reset14Button').addEventListener(
+  'click',
+  function () {
+    reset14();
+    render();
+  }
+);
+document.getElementById('discrement14Button').addEventListener(
+  'click',
+  function () {
+    discrement14();
+    render();
+  }
+);
+const state15 = {
+  current: 0
+};
+function increment15() {
+  state15.current++;
+}
+function reset15() {
+  state15.current = 0;
+}
+function discrement15() {
+  state15.current--;
+}
+
+
+
+document.getElementById('increment15Button').addEventListener(
+  'click',
+  function () {
+    increment15();
+    render();
+  }
+);
+
+document.getElementById('reset15Button').addEventListener(
+  'click',
+  function () {
+    reset15();
+    render();
+  }
+);
+document.getElementById('discrement15Button').addEventListener(
+  'click',
+  function () {
+    discrement15();
+    render();
+  }
+);
+function render() {
+  const point1 = document.getElementById('point1');
+  point1.innerText = state1.current;
+  console.log(state1.current);
+  const point = document.getElementById('point');
+  point.innerText = state.current;
+  console.log(state.current);
+  const point2 = document.getElementById('point2');
+  point2.innerText = state2.current;
+  console.log(state2.current);
+  const point3 = document.getElementById('point3');
+  point3.innerText = state3.current;
+  console.log(state3.current);
+  const point4 = document.getElementById('point4');
+  point4.innerText = state4.current;
+  console.log(state4.current);
+  const point5 = document.getElementById('point5');
+  point5.innerText = state5.current;
+  console.log(state5.current);
+  const point6 = document.getElementById('point6');
+  point6.innerText = state6.current;
+  console.log(state6.current);
+  const point7 = document.getElementById('point7');
+  point7.innerText = state7.current;
+  console.log(state7.current);
+  const point8 = document.getElementById('point8');
+  point8.innerText = state8.current;
+  console.log(state8.current);
+  const point9 = document.getElementById('point9');
+  point9.innerText = state9.current;
+  console.log(state9.current);
+  const point10 = document.getElementById('point10');
+  point10.innerText = state10.current;
+  console.log(state10.current);
+  const point11 = document.getElementById('point11');
+  point11.innerText = state11.current;
+  console.log(state11.current);
+  const point12 = document.getElementById('point12');
+  point12.innerText = state12.current;
+  console.log(state12.current);
+  const point13 = document.getElementById('point13');
+  point13.innerText = state13.current;
+  console.log(state13.current);
+  const point14 = document.getElementById('point14');
+  point14.innerText = state14.current;
+  console.log(state14.current);
+  const point15 = document.getElementById('point15');
+  point15.innerText = state15.current;
+  console.log(state15.current);
+}
